10e15226212334bd70cd307981704a552e3b370f
chyerin802 <chyerin802@gmail.com>
2023-05-03 17:43:33 +0900

    feat: 검색어 input onChange 핸들러 함수 구현 #1

--- src/components/SearchInputBox.tsx
+++ src/components/SearchInputBox.tsx
@@ -1,8 +1,14 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState, ChangeEvent } from 'react';
 import RecommendedItemList from './RecommendedItemList';
 
 function SearchInputBox() {
   const searchInputRef = useRef<HTMLInputElement>(null);
+  const [searchWord, setSearchWord] = useState('');
+
+  // 검색어 input값의 변화를 다루는 onChange 핸들러 함수
+  const changeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchWord(event.target.value);
+  };
 
   // 처음 렌더링 시 input 창에 focus
   useEffect(() => {
@@ -13,9 +19,10 @@ function SearchInputBox() {
     <div>
       <form>
         <label htmlFor="word">search icon</label>
-        <input type="text" id="word" ref={searchInputRef} />
+        <input type="text" id="word" ref={searchInputRef} value={searchWord} onChange={changeHandler} />
         <button type="submit">검색</button>
       </form>
+      <div>{searchWord}</div>
       <RecommendedItemList />
     </div>
   );
