c0d57a93e981aefa5aa2008d40dee47ab032b6e1
chyerin802 <chyerin802@gmail.com>
2023-05-04 02:08:44 +0900

    fix: api 요청 파라미터명 수정 #4

--- src/api/recommendedItem.ts
+++ src/api/recommendedItem.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const GET_REC_ITEMS_ENDPOINT = '/api/v1/search-conditions/';
-const getRecommendedItemListAPI = async (word: string) => {
+const getRecommendedItemListAPI = async (name: string) => {
   try {
-    const res = await axios.get(GET_REC_ITEMS_ENDPOINT, { params: { name: word } });
+    const res = await axios.get(GET_REC_ITEMS_ENDPOINT, { params: { name } });
     return res.data;
   } catch (e) {
     console.error(e);
