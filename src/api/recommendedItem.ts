7f4c09b559760cb5ac92afec08d17f7e64b79402
chyerin802 <chyerin802@gmail.com>
2023-05-04 02:06:43 +0900

    fix: api endpoint 오류 수정

--- src/api/recommendedItem.ts
+++ src/api/recommendedItem.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const GET_REC_ITEMS_ENDPOINT = '/api/v1/search-conditions';
+const GET_REC_ITEMS_ENDPOINT = '/api/v1/search-conditions/';
 const getRecommendedItemListAPI = async (word: string) => {
   try {
     const res = await axios.get(GET_REC_ITEMS_ENDPOINT, { params: { name: word } });
