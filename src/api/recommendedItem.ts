f34ddbcfd674500db89fe27fb01f3df1b8fadb74
chyerin802 <chyerin802@gmail.com>
2023-05-04 00:17:36 +0900

    feat: api 호출을 위한 proxy 설정 (#4)

--- src/api/recommendedItem.ts
+++ src/api/recommendedItem.ts
@@ -1,22 +1,11 @@
-// eslint-disable-next-line no-promise-executor-return
-const wait = (timeToDelay: number) => new Promise(resolve => setTimeout(resolve, timeToDelay));
+import axios from 'axios';
 
-const dummyItems = [
-  {
-    name: '갑상선암',
-    id: 4373,
-  },
-  {
-    name: '갑상선염',
-    id: 4376,
-  },
-];
-
+const GET_REC_ITEMS_ENDPOINT = '/api/v1/search-conditions';
 const getRecommendedItemListAPI = async (word: string) => {
-  console.log(word, ' 가져올거임');
   try {
-    await wait(3000);
-    return dummyItems;
+    const res = await axios.get(GET_REC_ITEMS_ENDPOINT, { params: { name: word } });
+    console.log(res.data);
+    return res.data;
   } catch (e) {
     console.error(e);
     throw e;
