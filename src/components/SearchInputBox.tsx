0d008fc71d1a764461c7c593d21c6e706ca457ed
chyerin802 <chyerin802@gmail.com>
2023-05-03 17:32:30 +0900

    feat: 처음 렌더링 시 input 창에 focus 구현 #1

--- src/components/SearchInputBox.tsx
+++ src/components/SearchInputBox.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import RecommendedItemList from './RecommendedItemList';
 
 function SearchInputBox() {
+  const searchInputRef = useRef<HTMLInputElement>(null);
+
+  // 처음 렌더링 시 input 창에 focus
+  useEffect(() => {
+    searchInputRef.current!.focus();
+  }, []);
+
   return (
     <div>
       <form>
         <label htmlFor="word">search icon</label>
-        <input type="text" id="word" />
+        <input type="text" id="word" ref={searchInputRef} />
         <button type="submit">검색</button>
       </form>
       <RecommendedItemList />
