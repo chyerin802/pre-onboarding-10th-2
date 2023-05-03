612e8faedafebaec0aabdfdce2942aae8d42f517
chyerin802 <chyerin802@gmail.com>
2023-05-04 02:11:59 +0900

    feat: api 요청 횟수 표출 #4

--- src/api/recommendedItem.ts
+++ src/api/recommendedItem.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const GET_REC_ITEMS_ENDPOINT = '/api/v1/search-conditions/';
 const getRecommendedItemListAPI = async (name: string) => {
+  console.info('calling api');
   try {
     const res = await axios.get(GET_REC_ITEMS_ENDPOINT, { params: { name } });
     return res.data;
