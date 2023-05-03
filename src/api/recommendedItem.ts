3ec64075012f33970bbde866bf2361483fdaf7b2
chyerin802 <chyerin802@gmail.com>
2023-05-04 01:36:21 +0900

    style: 불필요한 콘솔 로그 삭제

--- src/api/recommendedItem.ts
+++ src/api/recommendedItem.ts
@@ -4,7 +4,6 @@ const GET_REC_ITEMS_ENDPOINT = '/api/v1/search-conditions';
 const getRecommendedItemListAPI = async (word: string) => {
   try {
     const res = await axios.get(GET_REC_ITEMS_ENDPOINT, { params: { name: word } });
-    console.log(res.data);
     return res.data;
   } catch (e) {
     console.error(e);
