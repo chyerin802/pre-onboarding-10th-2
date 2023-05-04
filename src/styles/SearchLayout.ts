5c5490752df9b6aa61d5715c6fdb32c702bbc2d2
chyerin802 <chyerin802@gmail.com>
2023-05-04 13:50:58 +0900

    design: SearchBar 스타일 적용 #10

---
+++ src/styles/SearchLayout.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+const SearchLayOut = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  padding-top: 100px;
+  background-color: var(--bg-color);
+
+  > h1 {
+    font-size: 2.125rem;
+    font-weight: 700;
+    letter-spacing: -0.018em;
+    line-height: 1.6;
+    margin-bottom: 20px;
+    text-align: center;
+  }
+`;
+
+export default SearchLayOut;
