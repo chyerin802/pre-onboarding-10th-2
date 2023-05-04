0dd23a53d594bd47e8ffd46a6b561140537b594d
chyerin802 <chyerin802@gmail.com>
2023-05-04 09:10:09 +0900

    refactor: localStorage에 유효 시간이 지정된 데이터 배열 저장하는 함수 분리 #6

--- src/utils/cacheRecItem.ts
+++ src/utils/cacheRecItem.ts
@@ -1,46 +1,21 @@
 import { RecItem } from '@type/recommendedItem';
-import { getLocalStorageItem, setLocalStorageItem } from './localStorage';
+import { getLocalItemListWithExpiry, setLocalItemListWithExpiry } from './localStorage';
 
 const CACHE_KEY_NAME = 'cachedRecItems';
-const CACHE_EXPIRE_TIME = 500;
+const CACHE_EXPIRE_TIME = 10000;
 
-type CachedRecItemsType = {
+type CachedRecItem = {
   word: string;
   data: RecItem[];
-  expiry: number;
 };
 
 const getCachedRecItems = (word: string) => {
-  const localCachedItems = getLocalStorageItem(CACHE_KEY_NAME);
-
-  if (localCachedItems) {
-    const cachedItems: CachedRecItemsType[] = JSON.parse(localCachedItems);
-    const findItem = cachedItems?.find((item: CachedRecItemsType) => item.word === word);
-
-    if (!findItem) return;
-
-    const now = new Date();
-    console.log('now', now.getTime());
-    console.log('expiry', findItem.expiry);
-
-    if (now.getTime() > findItem.expiry) {
-      setLocalStorageItem(
-        CACHE_KEY_NAME,
-        cachedItems.filter(item => item.word !== findItem.word)
-      );
-    } else {
-      return findItem.data;
-    }
-  }
+  const getValue = getLocalItemListWithExpiry(CACHE_KEY_NAME, (value: CachedRecItem) => value.word === word);
+  return getValue?.data;
 };
 
 const setCachedRecItems = (word: string, data: RecItem[]) => {
-  const localCachedItems = getLocalStorageItem(CACHE_KEY_NAME);
-  let cachedItems: CachedRecItemsType[] = [];
-  const now = new Date();
-
-  if (localCachedItems) cachedItems = JSON.parse(localCachedItems);
-  setLocalStorageItem(CACHE_KEY_NAME, [...cachedItems, { word, data, expiry: now.getTime() + CACHE_EXPIRE_TIME }]);
+  setLocalItemListWithExpiry(CACHE_KEY_NAME, { word, data }, CACHE_EXPIRE_TIME);
 };
 
 export { getCachedRecItems, setCachedRecItems };
